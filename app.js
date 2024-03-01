5129c611be982a35f8a3beea632ade825e10e787
tankist2133 <57206743+tankist2133@users.noreply.github.com>
2024-03-01 23:07:02 +0700

    new design and functionality

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const getPhoto = document.getElementById('getPhoto');
 const sendResponse = document.getElementById('sendResponse');
+const keyWorld = document.getElementById('keyWorld')
 let img = document.createElement('img');
 
 
@@ -9,8 +10,8 @@ let img = document.createElement('img');
 sendResponse.addEventListener('click', () => {
     const xhr = new XMLHttpRequest();
 
-    let query = 'город деревья';
-    let url = 'https://api.unsplash.com/photos/random?w=700&h500&page=1&query='+query;
+    let query = keyWorld.value;
+    let url = 'https://api.unsplash.com/photos/random?w=200&h200&page=1&query='+query;
     const YOUR_ACCESS_KEY = '9pRLp2nFjb4ao-xk4VZCTOOgRR9tn0I36NJrxMbHrZ4';
 
     xhr.open('GET', url);
@@ -23,6 +24,7 @@ sendResponse.addEventListener('click', () => {
             let imgUrl = data['links']['download'];
             img.src = imgUrl;
             getPhoto.appendChild(img);
+            console.log('Запрос:' + query)
 
         } else {
             console.error("Ошибка запроса: " + xhr.status)
